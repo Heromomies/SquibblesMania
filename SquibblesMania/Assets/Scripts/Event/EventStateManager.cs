26a385c7674c0c876876b623f7ec38a30afb2488
Loann <courcoul66@gmail.com>
2021-12-10 14:19:13 +0100

    Start Pattern for the event of the volcanic island

--- SquibblesMania/Assets/Scripts/Event/EventStateManager.cs
+++ SquibblesMania/Assets/Scripts/Event/EventStateManager.cs
@@ -10,17 +10,48 @@ public class EventStateManager : MonoBehaviour
     private SmokeAsheState _smokeAsheState= new SmokeAsheState();
     private RiverState _riverState= new RiverState();
 
+    public GameObject meteorite;
+
+    #region Singleton
+
+    private static EventStateManager _instance;
+
+    public static EventStateManager Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<EventStateManager>();
+            }
+
+            return _instance;
+        }
+    }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }    
+
+    #endregion
+    
     // Start is called before the first frame update
     void Start()
     {
-        _currentState = _eruptionState;
         
-        _currentState.EnterState(this);
     }
 
     // Update is called once per frame
     void Update()
     {
-        _currentState.UpdateState(this);
+        
+    }
+
+    public void OnClick()
+    {
+        _currentState = _eruptionState;
+        
+        _currentState.EnterState(this);
     }
 }
---
+++ SquibblesMania/Assets/Scripts/Event/Pattern/EventScriptableObject.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "Scriptable Object", menuName = "Scriptable Object / Volcanic Island")]
+public class EventScriptableObject : ScriptableObject
+{
+      public GameObject objectToSpawn;
+      
+}
