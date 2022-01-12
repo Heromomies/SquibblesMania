f98b71f26707a95d6c10963038083410c97db371
Loann <courcoul66@gmail.com>
2022-01-12 15:58:55 +0100

    Add PowerManager on players prefab

--- SquibblesMania/Assets/Scripts/Power/PowerManager.cs
+++ SquibblesMania/Assets/Scripts/Power/PowerManager.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 
 public class PowerManager : MonoBehaviour
 {
-    public GameObject player;
     public int maxDistance;
 
     private float _height;
@@ -15,21 +14,19 @@ public class PowerManager : MonoBehaviour
     {
         RaycastHit hit;
         
-        Debug.DrawRay(player.transform.position, Vector3.back * maxDistance, Color.yellow);
+        Debug.DrawRay(transform.transform.position, Vector3.back * maxDistance, Color.yellow);
         
-        if (Physics.Raycast(player.transform.position, Vector3.back, out hit, maxDistance))
+        if (Physics.Raycast(transform.transform.position, Vector3.back, out hit, maxDistance))
         {
             var position = hit.transform.position;
-            Debug.Log(position.y);
             _height = position.y;
         }
     }
 
     public void AttractPlayer()
     {
-        var transformPosition = player.transform.position;
-        player.transform.position += Vector3.back;
-        transformPosition.y += transformPosition.y + _height; 
-        
+        var transformPosition = transform.position;
+        transform.position += Vector3.back;
+        transformPosition.y += transformPosition.y + _height;
     }
 }
