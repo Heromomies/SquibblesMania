533115640550618a74eaebc9b843808d64f638d8
Loann <courcoul66@gmail.com>
2022-01-13 09:41:14 +0100

    Remove power manager from players to have only one manager, creation of an interface to manage the powers

--- SquibblesMania/Assets/Scripts/Power/PowerManager.cs
+++ SquibblesMania/Assets/Scripts/Power/PowerManager.cs
@@ -9,37 +9,44 @@ public class PowerManager : MonoBehaviour
     public int maxDistance;
 
     private float _height;
-    private bool _canTouch;
+    private bool _isOne, _isTwo, _isThree, _isFour;
     private void Update()
     {
         RaycastHit hit;
         
-        Debug.DrawRay(transform.position, Vector3.back * maxDistance, Color.yellow);
+      /*  Debug.DrawRay(transform.position, Vector3.back * maxDistance, Color.yellow);
         Debug.DrawRay(transform.position, Vector3.forward * maxDistance, Color.yellow);
         Debug.DrawRay(transform.position, Vector3.left * maxDistance, Color.yellow);
-        Debug.DrawRay(transform.position, Vector3.right * maxDistance, Color.yellow);
+        Debug.DrawRay(transform.position, Vector3.right * maxDistance, Color.yellow);*/
         
-        if (Physics.Raycast(transform.position, Vector3.back, out hit, maxDistance) && !_canTouch)
+        if (Physics.Raycast(transform.position, Vector3.back, out hit, maxDistance) && !_isOne && 
+            Input.GetMouseButtonDown(0) && GameManager.Instance.currentPlayerTurn.name == gameObject.name)
         {
-            _canTouch = true;
+            Debug.Log(GameManager.Instance.currentPlayerTurn);
+            _isOne = true;
             hit.transform.position += Vector3.back;
+            Debug.DrawRay(transform.position, Vector3.back * maxDistance, Color.red);
             Debug.Log("I'm touching a player from the back");
         }
-        if (Physics.Raycast(transform.position, Vector3.forward, out hit, maxDistance) && !_canTouch)
+        if (Physics.Raycast(transform.position, Vector3.forward, out hit, maxDistance) && !_isTwo && 
+            Input.GetMouseButtonDown(0) && GameManager.Instance.currentPlayerTurn.name == gameObject.name)
         {
-            _canTouch = true;
-            hit.transform.position += Vector3.forward;
+            _isTwo = true;
+            hit.transform.position -= Vector3.back;
+            Debug.DrawRay(transform.position, Vector3.forward * maxDistance, Color.red);
             Debug.Log("I'm touching a player from forward");
         }
-        if (Physics.Raycast(transform.position, Vector3.left, out hit, maxDistance) && !_canTouch)
+        if (Physics.Raycast(transform.position, Vector3.left, out hit, maxDistance) && !_isThree && 
+            Input.GetMouseButtonDown(0)&& GameManager.Instance.currentPlayerTurn.name == gameObject.name)
         {
-            _canTouch = true;
+            _isThree = true;
             hit.transform.position += Vector3.left;
             Debug.Log("I'm touching a player from the left");
         }
-        if (Physics.Raycast(transform.position, Vector3.right, out hit, maxDistance) && !_canTouch)
+        if (Physics.Raycast(transform.position, Vector3.right, out hit, maxDistance) && !_isFour && 
+            Input.GetMouseButtonDown(0)&& GameManager.Instance.currentPlayerTurn.name == gameObject.name)
         {
-            _canTouch = true;
+            _isFour = true;
             hit.transform.position += Vector3.right;
             Debug.Log("I'm touching a player from the right");
         }
