1c44ae89a97bad309240d71ef29a229332a69c15
Loann <courcoul66@gmail.com>
2022-02-10 14:59:04 +0100

    AshesSmoke Event made

--- SquibblesMania/Assets/Scripts/Event/Volcanic Island/AshesSmoke/AshesSmoke.cs
+++ SquibblesMania/Assets/Scripts/Event/Volcanic Island/AshesSmoke/AshesSmoke.cs
@@ -1,38 +1,69 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class AshesSmoke : MonoBehaviour, IManageEvent
 {
 	[Header("EVENT")] private List<GameObject> _cubeOnMap;
 	[HideInInspector] public List<GameObject> cubeTouched;
-	public GameObject eventParticle;
+	public List<GameObject> parentBlocs;
 
-	public int numberOfSmoke;
+	public GameObject ashes;
 	public float heightSpawnParticle;
 	private int _cubeToChange;
+	public List<GameObject> childrenBlocs;
+	public Conditions[] conditionsDangerousnessAshesSmoke;
+
+	[Serializable]
+	public struct Conditions
+	{
+		public int numberOfBlocsCovered;
+	}
+	
+	private void OnEnable()
+	{
+		ShowEvent();
+	}
+
+	/// <summary>
+	/// To random a list easily
+	/// </summary>
+	/*void Shuffle<T>(List<T> inputList)
+	{
+		for (int i = 0; i < inputList.Count; i++)
+		{
+			T temp = inputList[i];
+			int random = Random.Range(1, inputList.Count);
+			inputList[i] = inputList[random];
+			inputList[random] = temp;
+		}
+	}*/
 	
 	public void ShowEvent() // Show the event
 	{
-		_cubeOnMap = EventManager.Instance.cleanList;
-		Debug.Log(_cubeOnMap.Count);
-		
-		_cubeToChange = Random.Range(0, _cubeOnMap.Count - numberOfSmoke);
-
-		for (int i = 0; i < numberOfSmoke; i++) // Allow to colorize material to create a compact zone
+		foreach (var t in parentBlocs)
 		{
-			_cubeOnMap[_cubeToChange + i].GetComponent<Renderer>().material.color = Color.black;
-			cubeTouched.Add(_cubeOnMap[_cubeToChange + i]);
-		} 
+			foreach (var child in t.GetComponentsInChildren<Node>())
+			{
+				childrenBlocs.Add(child.gameObject);
+			}
+		}
+
+		int rand = Random.Range(0, childrenBlocs.Count 
+		                           - conditionsDangerousnessAshesSmoke[EventManager.Instance.dangerousness].numberOfBlocsCovered);
+
+		
+		for (int i = 0; i < conditionsDangerousnessAshesSmoke[EventManager.Instance.dangerousness].numberOfBlocsCovered; i++)
+		{
+			var c = childrenBlocs[i + rand].transform.position;
+			Instantiate(ashes, new Vector3(c.x, c.y + 0.55f, c.z), Quaternion.identity);
+		}
 	}
 
 	public void LaunchEvent() // Launch the event
 	{
-		foreach (var cubeTouched in cubeTouched) // Instantiate particle on the top of the block
-		{
-			float y = cubeTouched.transform.localScale.y;
-			
-			Instantiate(eventParticle, new Vector3(cubeTouched.transform.position.x, y+heightSpawnParticle, cubeTouched.transform.position.z), Quaternion.identity);
-		}
+		
 	}
 }
