0d3c5421bf4c6ece6c9c504398d48d8eccfb6590
Timothé <rondeautimothe@gmail.com>
2022-02-16 10:17:44 +0100

    PlayerCardState update

--- SquibblesMania/Assets/Scripts/Items/Ressources.cs
+++ SquibblesMania/Assets/Scripts/Items/Ressources.cs
@@ -37,7 +37,6 @@ public class Ressources : Item
             var player = other.gameObject.GetComponent<PlayerStateManager>();
             if (!playersList.Contains(player))
             {
-                Debug.Log(player);
                 playersList.Add(player);
                 TeamInventoryManager.Instance.AddResourcesToInventory(this, player.playerTeam);
                 var particle = Instantiate(particleSystemPrefab, transform.position, Quaternion.identity);
