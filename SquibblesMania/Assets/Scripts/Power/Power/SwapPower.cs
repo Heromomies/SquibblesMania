73af52742a1155f37433bdd58bbf6340fb6ebab0
Loann <courcoul66@gmail.com>
2022-02-18 10:29:47 +0100

    Fix bug on touch swap power

--- SquibblesMania/Assets/Scripts/Power/Power/SwapPower.cs
+++ SquibblesMania/Assets/Scripts/Power/Power/SwapPower.cs
@@ -1,82 +1,100 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
+using DigitalRubyShared;
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
-using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class SwapPower : MonoBehaviour
 {
 	public int range;
 	public LayerMask layer;
+
+	public Collider[] players;
 	
 	private GameObject _playerToSwap;
 	private Vector3 _pos;
 	private Collider _playerOne, _playerTwo;
 	private bool _canChoseThePlayer;
 	private Camera _cam;
-	
-	public void OnEnable()
+
+	private readonly List<RaycastResult> raycast = new List<RaycastResult>();
+	public PanGestureRecognizer PlayerTouchGesture { get; private set; }
+
+	private void Awake()
 	{
 		_cam = Camera.main;
+	}
+
+	private void OnEnable()
+	{
+		PlayerTouchGesture = new PanGestureRecognizer();
+		PlayerTouchGesture.ThresholdUnits = 0.0f; // start right away
+		//Add new gesture
+		PlayerTouchGesture.StateUpdated += PlayerTouchGestureUpdated;
+		PlayerTouchGesture.AllowSimultaneousExecutionWithAllGestures();
+
+		FingersScript.Instance.AddGesture(PlayerTouchGesture);
+		
 		_playerToSwap = GameManager.Instance.currentPlayerTurn.gameObject;
 		transform.position = _playerToSwap.transform.position;
 		ShowPower();
 	}
-	
-	public void ShowPower() // Show the sphere and admit the player to chose the other player to swap
-	{
-		Collider[] hitColliders = Physics.OverlapSphere(transform.position, range, layer);
 
-		switch (hitColliders.Length)
+	private void ShowPower() // Show the sphere and admit the player to chose the other player to swap
+	{
+		players = Physics.OverlapSphere(transform.position, range, layer);
+
+		_playerOne = GameManager.Instance.currentPlayerTurn.gameObject.GetComponent<Collider>();
+		
+		switch (players.Length)
 		{
 			case 1:
-				PowerManager.Instance.ActivateDeactivatePower(2,false);
+				PowerManager.Instance.ActivateDeactivatePower(2, false);
 				PowerManager.Instance.ChangeTurnPlayer();
 				break;
-			case 2:
-				_playerOne = hitColliders[0];
-				_playerTwo = hitColliders[1];
-				LaunchPower();
-				break;
-		}
-
-		if (hitColliders.Length >= 3)
-		{
-			_canChoseThePlayer = true;
 		}
 	}
 
-	private void Update()
+	private void PlayerTouchGestureUpdated(GestureRecognizer gesture)
 	{
-		if (_canChoseThePlayer)
+		if (gesture.State == GestureRecognizerState.Began)
 		{
-			if (Input.touchCount > 0)
+			PointerEventData p = new PointerEventData(EventSystem.current);
+			p.position = new Vector2(gesture.FocusX, gesture.FocusY);
+
+			raycast.Clear();
+			EventSystem.current.RaycastAll(p, raycast);
+
+			Ray ray = _cam.ScreenPointToRay(p.position);
+
+			if (Physics.Raycast(ray, out var hitInfo, Mathf.Infinity, layer))
 			{
-				if (Input.GetTouch(0).phase == TouchPhase.Began)
-				{
-					Touch touch = Input.GetTouch(0);
-					//ray shooting out of the camera from where the touch is
+				_playerTwo = hitInfo.collider;
 
-					_playerTwo.transform.position = _cam.ScreenToWorldPoint(touch.position);
-
-					LaunchPower();
-				}
+				LaunchPower();
+			}
+			else
+			{
+				gesture.Reset();
 			}
 		}
 	}
 	
-	public void LaunchPower()
+	private void LaunchPower()
 	{
-		_pos = _playerOne.transform.position;
-		SwapPosition(_playerOne.transform, _playerTwo.transform);
+		var transformPlayerOne = _playerOne.transform;
+		_pos = transformPlayerOne.position;
+		SwapPosition(transformPlayerOne, _playerTwo.transform);
 	}
 
-	void SwapPosition(Transform playerOne, Transform playerTwo) // Swap the position between the two players
+	private void SwapPosition(Transform playerOne, Transform playerTwo) // Swap the position between the two players
 	{
 		playerOne.position = playerTwo.position;
 		playerTwo.position = _pos;
-		PowerManager.Instance.ActivateDeactivatePower(2,false);
+		
+		PlayerTouchGesture.StateUpdated -= PlayerTouchGestureUpdated;
+		
+		PowerManager.Instance.ActivateDeactivatePower(2, false);
 		PowerManager.Instance.ChangeTurnPlayer();
 	}
 }
