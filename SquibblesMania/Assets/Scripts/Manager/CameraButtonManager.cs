fbfc7f1d0a4639b8cd33a119e18cf6374ef8ce30
Timothé <rondeautimothe@gmail.com>
2022-03-11 15:44:12 +0100

    Cam button manager

--- SquibblesMania/Assets/Scripts/Manager/CameraButtonManager.cs
+++ SquibblesMania/Assets/Scripts/Manager/CameraButtonManager.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using DigitalRubyShared;
 using UnityEngine;
 using UnityEngine.UIElements;
+using Button = UnityEngine.UI.Button;
 
 public class CameraButtonManager : MonoBehaviour
 {
@@ -12,17 +14,50 @@ public class CameraButtonManager : MonoBehaviour
 
     private static CameraButtonManager _cameraManager;
     public static CameraButtonManager Instance => _cameraManager;
+    [SerializeField] private bool isCamRotate;
 
-
+    private float _timeRotateSpeedInSeconds = 0.5f;
+    [SerializeField]
+    private Button[] buttonsCamRotate;
     private void Awake()
     {
         _cameraManager = this;
         _cam = Camera.main.transform;
     }
 
-    public void RotateCam(int rotateCount)
+    public void StartRotateCam(float rotateAmount)
     {
-       
+        StartCoroutine(RotateCamCoroutine(rotateAmount));
+        foreach (var button in buttonsCamRotate)
+        {
+            button.enabled = !button.enabled;
+        }
+    }
+
+    private IEnumerator RotateCamCoroutine(float rotateAmount)
+    {
+        float timeSinceStarted = 0f;
+        if (ActualCamPreset.CamPresetTeam() == ActualCamPreset.Team.TeamOne)
+        {
+            while (true)
+            {
+                timeSinceStarted += Time.deltaTime * _timeRotateSpeedInSeconds;
+                _cam.transform.RotateAround(target.transform.position, Vector3.up, rotateAmount * Time.deltaTime);
+                // If the object has arrived, stop the coroutine
+                if (timeSinceStarted >= 0.45f)
+                {
+                    Debug.Log("Object arrived");
+                    foreach (var button in buttonsCamRotate)
+                    {
+                        button.enabled = !button.enabled;
+                    }
+                    yield break;
+                }
+
+                // Otherwise, continue next frame
+                yield return null;
+            }
+        }
     }
 
     public void TopViewMode()
