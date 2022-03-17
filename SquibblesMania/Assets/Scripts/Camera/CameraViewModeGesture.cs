c66197877f3ba7d9572685c63b90797f0cc71e2c
Timothé <rondeautimothe@gmail.com>
2022-03-17 21:49:47 +0100

    Camera view mode gesture testing

--- SquibblesMania/Assets/Scripts/Camera/CameraViewModeGesture.cs
+++ SquibblesMania/Assets/Scripts/Camera/CameraViewModeGesture.cs
@@ -12,11 +12,13 @@ public class CameraViewModeGesture : MonoBehaviour
     private Camera _cam;
     private LongPressGestureRecognizer _longPressViewModeGesture = new LongPressGestureRecognizer();
     private SwipeGestureRecognizer _swipeViewModeGesture = new SwipeGestureRecognizer();
-    [Header("SWIPE SETTINGS")]
-    [Range(1, 10)] public int swipeTouchCount = 1;
-    [Range(0.0f, 10.0f)] public float swipeThresholdSeconds;
-    [Range(0.0f, 1.0f)] public float minimumDistanceUnits = 0.2f;
-    
+
+    [Header("SWIPE SETTINGS")] [Range(1, 10), SerializeField]
+    private int swipeTouchCount = 1;
+
+    [Range(0.0f, 10.0f), SerializeField] private float swipeThresholdSeconds;
+    [Range(0.0f, 1.0f), SerializeField] private float minimumDistanceUnits = 0.2f;
+
     public UIViewMode actualUiViewMode;
     public List<UIViewMode> uiViewModeList;
 
@@ -32,10 +34,11 @@ public class CameraViewModeGesture : MonoBehaviour
     }
 
     private WaitForSeconds _timeInSecondsForSwitchViewMode = new WaitForSeconds(0.3f);
-    [Header("CURSOR SETTINGS")] private Transform _currentSelectedCursor;
-    private Transform _currentCircleTransform;
+    [Header("CURSOR SETTINGS")] 
     [SerializeField] private bool isCursorSelected;
-    [SerializeField] private float scaleAmount = 1.2f;
+    [SerializeField] private float scaleCursorAmount = 1.2f;
+    private Transform _currentSelectedCursor;
+    private Transform _currentCircleTransform;
     private List<RaycastResult> _raycast = new List<RaycastResult>();
     [SerializeField] private List<RectTransform> uiCircleRectTransforms;
     private RaycastHit _hit;
@@ -52,14 +55,15 @@ public class CameraViewModeGesture : MonoBehaviour
         _swipeViewModeGesture = new SwipeGestureRecognizer();
         _swipeViewModeGesture.StateUpdated += SwipeViewModeGestureOnStateUpdated;
         _swipeViewModeGesture.DirectionThreshold = 0;
-        _swipeViewModeGesture.MinimumNumberOfTouchesToTrack = _swipeViewModeGesture.MaximumNumberOfTouchesToTrack = swipeTouchCount;
+        _swipeViewModeGesture.MinimumNumberOfTouchesToTrack =
+            _swipeViewModeGesture.MaximumNumberOfTouchesToTrack = swipeTouchCount;
         _swipeViewModeGesture.ThresholdSeconds = swipeThresholdSeconds;
         _swipeViewModeGesture.MinimumDistanceUnits = minimumDistanceUnits;
         _swipeViewModeGesture.EndMode = SwipeGestureRecognizerEndMode.EndContinusously;
         _swipeViewModeGesture.AllowSimultaneousExecution(_longPressViewModeGesture);
         FingersScript.Instance.AddGesture(_swipeViewModeGesture);
-        
-        
+
+
         //Set up the new gesture 
         _longPressViewModeGesture = new LongPressGestureRecognizer();
         _longPressViewModeGesture.PlatformSpecificView = actualUiViewMode.uiCursorSelection;
@@ -67,49 +71,6 @@ public class CameraViewModeGesture : MonoBehaviour
         _longPressViewModeGesture.ThresholdUnits = 0.0f;
         _longPressViewModeGesture.MinimumDurationSeconds = 0.2f;
         FingersScript.Instance.AddGesture(_longPressViewModeGesture);
-    }
-
-    private void SwipeViewModeGestureOnStateUpdated(GestureRecognizer gesture)
-    {
-        SwipeGestureRecognizer swipeGestureRecognizer = gesture as SwipeGestureRecognizer;
-        if (gesture.State == GestureRecognizerState.Ended)
-        {
-            if (isCursorSelected)
-            {
-                var cursorEulerAngles = _currentSelectedCursor.eulerAngles;
-                if (!_currentCircleTransform)
-                {
-                    _currentCircleTransform = uiCircleRectTransforms[1].transform;
-                }
-                switch (swipeGestureRecognizer.EndDirection)
-                {
-                    case SwipeGestureRecognizerDirection.Up:
-                        if (_currentCircleTransform == uiCircleRectTransforms[1].transform)
-                        {
-                            StartCoroutine(StartMovementViewModeStateCoroutine(uiCircleRectTransforms[0].transform,  new Vector2(0,40),cursorEulerAngles, -90f));
-                        }
-                        else
-                        {
-                            StartCoroutine(StartMovementViewModeStateCoroutine(uiCircleRectTransforms[1].transform,  new Vector2(-56f,0),cursorEulerAngles));
-                        }
-                        break;
-                    case SwipeGestureRecognizerDirection.Down:
-                        if (_currentCircleTransform == uiCircleRectTransforms[0].transform)
-                        {
-                            StartCoroutine(StartMovementViewModeStateCoroutine(uiCircleRectTransforms[1].transform,  new Vector2(-56f,0),cursorEulerAngles ));
-                        }
-                        else
-                        {
-                            StartCoroutine(StartMovementViewModeStateCoroutine(uiCircleRectTransforms[2].transform,  new Vector2(-40,0),cursorEulerAngles));
-                        }
-                        break;
-                }
-               
-             
-               
-            }
-        }
-        
     }
 
     private void OnDisable()
@@ -120,6 +81,52 @@ public class CameraViewModeGesture : MonoBehaviour
             FingersScript.Instance.RemoveGesture(_swipeViewModeGesture);
         }
     }
+
+    private void SwipeViewModeGestureOnStateUpdated(GestureRecognizer gesture)
+    {
+        SwipeGestureRecognizer swipeGestureRecognizer = gesture as SwipeGestureRecognizer;
+        if (gesture.State == GestureRecognizerState.Ended && isCursorSelected)
+        {
+            var cursorEulerAngles = _currentSelectedCursor.eulerAngles;
+            var camIconEulerAngles = actualUiViewMode.uiCamIcon.transform.eulerAngles;
+            if (!_currentCircleTransform)
+            {
+                _currentCircleTransform = uiCircleRectTransforms[1].transform;
+                _currentCircleTransform.gameObject.SetActive(true);
+            }
+
+            switch (swipeGestureRecognizer.EndDirection)
+            {
+                case SwipeGestureRecognizerDirection.Up:
+                    if (_currentCircleTransform == uiCircleRectTransforms[1].transform)
+                    {
+                        StartCoroutine(StartMovementViewModeStateCoroutine(uiCircleRectTransforms[0].transform,
+                            new Vector2(0, 40), cursorEulerAngles, camIconEulerAngles, -90f, -90f));
+                    }
+                    else
+                    {
+                        StartCoroutine(StartMovementViewModeStateCoroutine(uiCircleRectTransforms[1].transform,
+                            new Vector2(-56f, 0), cursorEulerAngles, camIconEulerAngles, -30f));
+                    }
+
+                    break;
+                case SwipeGestureRecognizerDirection.Down:
+                    if (_currentCircleTransform == uiCircleRectTransforms[0].transform)
+                    {
+                        StartCoroutine(StartMovementViewModeStateCoroutine(uiCircleRectTransforms[1].transform,
+                            new Vector2(-56f, 0), cursorEulerAngles, camIconEulerAngles, -30f));
+                    }
+                    else
+                    {
+                        StartCoroutine(StartMovementViewModeStateCoroutine(uiCircleRectTransforms[2].transform,
+                            new Vector2(-40, 0), cursorEulerAngles, camIconEulerAngles, -10f));
+                    }
+
+                    break;
+            }
+        }
+    }
+
 
     private void LongPressViewModeGestureOnStateUpdated(GestureRecognizer gesture)
     {
@@ -145,7 +152,7 @@ public class CameraViewModeGesture : MonoBehaviour
 
             if (isCursorSelected)
             {
-                _currentSelectedCursor.transform.localScale = Vector3.one * scaleAmount;
+                _currentSelectedCursor.transform.localScale = Vector3.one * scaleCursorAmount;
                 foreach (var circle in actualUiViewMode.uiCircleSelection)
                 {
                     uiCircleRectTransforms.Add(circle.GetComponent<RectTransform>());
@@ -154,7 +161,7 @@ public class CameraViewModeGesture : MonoBehaviour
 
             _raycast.Clear();
         }
-        
+
         else if (gesture.State == GestureRecognizerState.Ended)
         {
             ClearStartState();
@@ -162,12 +169,21 @@ public class CameraViewModeGesture : MonoBehaviour
         }
     }
 
-    private IEnumerator StartMovementViewModeStateCoroutine(Transform uiCircleRectTransform, Vector2 offset, Vector3 cursorEulerAngles, float angleZ = 0f)
-    { 
+    private IEnumerator StartMovementViewModeStateCoroutine(Transform uiCircleRectTransform, Vector2 offset,
+        Vector3 cursorEulerAngles, Vector3 camIconEulerAngles, float camIconAngleZ = 0f, float angleZ = 0f)
+    {
+        if (_currentCircleTransform)
+        {
+            _currentCircleTransform.gameObject.SetActive(false);
+        }
         _currentCircleTransform = uiCircleRectTransform;
+        uiCircleRectTransform.gameObject.SetActive(true);
+        
         cursorEulerAngles.z = angleZ;
         _currentSelectedCursor.eulerAngles = cursorEulerAngles;
         _currentSelectedCursor.transform.position = uiCircleRectTransform.position + (Vector3)offset;
+        camIconEulerAngles.z = camIconAngleZ;
+        actualUiViewMode.uiCamIcon.eulerAngles = camIconEulerAngles;
         isCursorSelected = false;
         yield return _timeInSecondsForSwitchViewMode;
         isCursorSelected = true;
