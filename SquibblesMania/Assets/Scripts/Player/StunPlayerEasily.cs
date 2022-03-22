e502c5e3205e70172e593234806b15696149f9eb
Loann <courcoul66@gmail.com>
2022-03-22 16:07:59 +0100

    Stun player work

--- SquibblesMania/Assets/Scripts/Player/StunPlayerEasily.cs
+++ SquibblesMania/Assets/Scripts/Player/StunPlayerEasily.cs
@@ -2,16 +2,17 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 public class StunPlayerEasily : MonoBehaviour
 {
-    public GameObject stunPlayerMeteorite;
-
-    public void ClickButton()
+	public void Update()
     {
-	    var secondPlayer = GameManager.Instance.players[1];
-	    var posSp = secondPlayer.transform.position;
-	    Instantiate(stunPlayerMeteorite, new Vector3(posSp.x, posSp.y + 3f, posSp.z), Quaternion.identity);
-        
+	    if (Input.GetKeyDown(KeyCode.T))
+	    {
+		    var secondPlayer = GameManager.Instance.players[1];
+		    var posSp = secondPlayer.transform.position;
+		    PoolManager.Instance.SpawnObjectFromPool("Meteorite", new Vector3(posSp.x, posSp.y + 3f, posSp.z), Quaternion.identity, null);
+	    }
     }
 }
