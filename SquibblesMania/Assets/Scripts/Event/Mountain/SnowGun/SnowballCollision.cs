436009cf0f2f3e2dfec4f8ee33616e98fc551f84
Loann <courcoul66@gmail.com>
2022-04-27 10:55:52 +0200

    Add Breakable Ice

--- SquibblesMania/Assets/Scripts/Event/Mountain/SnowGun/SnowballCollision.cs
+++ SquibblesMania/Assets/Scripts/Event/Mountain/SnowGun/SnowballCollision.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 
 public class SnowballCollision : MonoBehaviour
 {
+    public GameObject breakableIce;
+    
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && other.name != GameManager.Instance.currentPlayerTurn.name)
         {
-            
+            Instantiate(breakableIce, other.transform.position + new Vector3(0,0.25f,0), Quaternion.identity);
+            gameObject.SetActive(false);
         }
     }
 }
