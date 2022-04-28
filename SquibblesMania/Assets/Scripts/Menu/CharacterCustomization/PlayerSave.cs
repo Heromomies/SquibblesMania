775e3c81ed909aa56387f9f512f6a0739e9baad0
Gryfs <Allan18040@gmail.com>
2022-04-28 11:44:02 +0200

    1 color by team

--- SquibblesMania/Assets/Scripts/Menu/CharacterCustomization/PlayerSave.cs
+++ SquibblesMania/Assets/Scripts/Menu/CharacterCustomization/PlayerSave.cs
@@ -9,6 +9,8 @@ public class PlayerSave : MonoBehaviour
     public GameObject P2;
     public GameObject P3;
     public GameObject P4;
+    public GameObject T1;
+    public GameObject T2;
 
     public PlayerData playerData;
 
@@ -16,16 +18,16 @@ public class PlayerSave : MonoBehaviour
 
     public void SaveData()
     {
-        playerData.P1colorID = P1.GetComponent<CharacterCustomization>().colorID;
+        playerData.P1colorID = T1.GetComponent<CharacterCustomization>().colorID;
         playerData.P1hatID = P1.GetComponent<CharacterCustomization>().hatID;
 
-        playerData.P2colorID = P2.GetComponent<CharacterCustomization>().colorID;
+        playerData.P2colorID = T1.GetComponent<CharacterCustomization>().colorID;
         playerData.P2hatID = P2.GetComponent<CharacterCustomization>().hatID;
 
-        playerData.P3colorID = P3.GetComponent<CharacterCustomization>().colorID;
+        playerData.P3colorID = T2.GetComponent<CharacterCustomization>().colorID;
         playerData.P3hatID = P3.GetComponent<CharacterCustomization>().hatID;
 
-        playerData.P4colorID = P4.GetComponent<CharacterCustomization>().colorID;
+        playerData.P4colorID = T2.GetComponent<CharacterCustomization>().colorID;
         playerData.P4hatID = P4.GetComponent<CharacterCustomization>().hatID;
 
         mapID = playerData.MapID;
