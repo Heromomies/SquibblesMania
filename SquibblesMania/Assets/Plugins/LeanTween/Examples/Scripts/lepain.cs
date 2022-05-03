70e5a31747a68ebf8a981ca02d5aa1364f998870
Gryfs <Allan18040@gmail.com>
2022-05-03 16:59:41 +0200

    Anim mirror

--- SquibblesMania/Assets/Plugins/LeanTween/Examples/Scripts/lepain.cs
+++ SquibblesMania/Assets/Plugins/LeanTween/Examples/Scripts/lepain.cs
@@ -7,14 +7,19 @@ using TMPro;
 public class PowerAnimation : MonoBehaviour
 {
     public GameObject dash;
+    private RectTransform dashTransform;
 
     public GameObject swap;
+    private RectTransform swapTransform1;
+    private RectTransform swapTransform2;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
-
+        dashTransform = dash.GetComponent<RectTransform>();
+        swapTransform1 = swap.transform.GetChild(0).GetComponent<RectTransform>();
+        swapTransform2 = swap.transform.GetChild(1).GetComponent<RectTransform>();
     }
 
     // Update is called once per frame
@@ -29,8 +34,8 @@ public class PowerAnimation : MonoBehaviour
 
     IEnumerator DashAnim()
     {
-        LeanTween.move(dash.GetComponent<RectTransform>(), new Vector3(0f, 0f, 0f), 0.2f);
-        LeanTween.move(dash.GetComponent<RectTransform>(), new Vector3(750f, 0f, 0f), 0.2f).setDelay(1f);
+        LeanTween.move(dashTransform, new Vector3(0f, 0f, 0f), 0.2f);
+        LeanTween.move(dashTransform, new Vector3(750f, 0f, 0f), 0.2f).setDelay(1f);
         yield return new WaitForSeconds(2);
         dash.GetComponent<RectTransform>().anchoredPosition = new Vector3(-750f, 0f, 0f);
 
@@ -40,19 +45,23 @@ public class PowerAnimation : MonoBehaviour
 
     IEnumerator SwapAnim()
     {
-        LeanTween.move(swap.transform.GetChild(0).GetComponent<RectTransform>(), new Vector3(-100f, 0f, 0f), 0.5f);
-        LeanTween.move(swap.transform.GetChild(1).GetComponent<RectTransform>(), new Vector3(100f, 0f, 0f), 0.5f);
+        LeanTween.move(swapTransform1, new Vector3(-100f, 0f, 0f), 0.3f);
+        LeanTween.move(swapTransform2, new Vector3(100f, 0f, 0f), 0.3f);
 
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(0.7f);
 
-        swap.transform.GetChild(1).GetComponent<TextMeshProUGUI>().color = Color.blue;
-        swap.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color = Color.red;
-        
+        swap.transform.GetChild(1).GetComponent<TextMeshProUGUI>().color = Color.Lerp(Color.blue, Color.red, 1);
+        swap.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color = Color.Lerp(Color.red, Color.blue, 1);
 
+        yield return new WaitForSeconds(0.5f);
 
-        yield return new WaitForSeconds(20);
-        dash.transform.GetChild(0).GetComponent<RectTransform>().anchoredPosition = new Vector3(-750f, 0f, 0f);
-        dash.transform.GetChild(1).GetComponent<RectTransform>().anchoredPosition = new Vector3(750f, 0f, 0f);
+        LeanTween.move(swapTransform1, new Vector3(-750f, 0f, 0f), 0.3f);
+        LeanTween.move(swapTransform2, new Vector3(750f, 0f, 0f), 0.3f);
+
+        yield return new WaitForSeconds(0.6f);
+
+        swap.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color = Color.blue;
+        swap.transform.GetChild(1).GetComponent<TextMeshProUGUI>().color = Color.red;
 
         yield return null;
     }
