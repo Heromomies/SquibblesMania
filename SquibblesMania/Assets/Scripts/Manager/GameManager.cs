d034a720c898f0074f7304e0b7d85612e213bf1b
Loann <courcoul66@gmail.com>
2022-05-06 11:46:00 +0200

    Can remove power's card

--- SquibblesMania/Assets/Scripts/Manager/GameManager.cs
+++ SquibblesMania/Assets/Scripts/Manager/GameManager.cs
@@ -285,15 +285,6 @@ public class GameManager : MonoBehaviour
         if(turnCount <= 0)
             turnCount=0;
 
-        if (count != 0)
-        {
-            currentPlayerTurn = players[count -1];
-        }
-        else if (count == 0)
-        {
-            currentPlayerTurn = players[3];
-        }
-        
         currentPlayerTurn.StartState();
     }    
     
