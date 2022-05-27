8d5206488618b3449c81c73742417be983db88c9
Timothé <rondeautimothe@gmail.com>
2022-05-27 10:12:33 +0200

    Anim text pop up with leantween

---
+++ SquibblesMania/Assets/Scripts/Ui/UiTextPopUpTween.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiTextPopUpTween : MonoBehaviour
+{
+    [SerializeField] private RectTransform textRectTransform;
+    [SerializeField] private float timeInSecondsScaleAnim = 0.3f;
+    [SerializeField] private LeanTweenType scaleEaseType;
+    private Vector3 _scaleDesired;
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        textRectTransform = GetComponent<RectTransform>();
+        _scaleDesired = textRectTransform.transform.localScale;
+    }
+
+#endif
+
+    private void OnEnable()
+    {
+        SetBaseScale(Vector3.zero);
+        LeanTween.scale(textRectTransform, _scaleDesired, timeInSecondsScaleAnim).setEase(scaleEaseType);
+    }
+
+    private void OnDisable()
+    {
+        SetBaseScale(Vector3.zero);
+    }
+
+    private void SetBaseScale(Vector3 scale)
+    {
+        textRectTransform.transform.localScale = scale;
+    }
+}
