981493843f9acc19156b007c020aeae12705b946
Timothé <rondeautimothe@gmail.com>
2022-05-27 11:49:04 +0200

    Update Menu with anim leantween

--- SquibblesMania/Assets/Scripts/Ui/UiPopUpTween.cs
+++ SquibblesMania/Assets/Scripts/Ui/UiPopUpTween.cs
@@ -3,17 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UiTextPopUpTween : MonoBehaviour
+public class UiPopUpTween : MonoBehaviour
 {
     [SerializeField] private RectTransform textRectTransform;
     [SerializeField] private float timeInSecondsScaleAnim = 0.3f;
     [SerializeField] private LeanTweenType scaleEaseType;
     private Vector3 _scaleDesired;
+
+    private Menu _menu;
 #if UNITY_EDITOR
     private void OnValidate()
     {
         textRectTransform = GetComponent<RectTransform>();
         _scaleDesired = textRectTransform.transform.localScale;
+        _menu = FindObjectOfType<Menu>();
     }
 
 #endif
@@ -22,8 +25,9 @@ public class UiTextPopUpTween : MonoBehaviour
     {
         SetScale(Vector3.zero);
         LeanTween.scale(textRectTransform, _scaleDesired, timeInSecondsScaleAnim).setEase(scaleEaseType);
-    }
 
+    }
+    
     private void OnDisable()
     {
         SetScale(Vector3.zero);
