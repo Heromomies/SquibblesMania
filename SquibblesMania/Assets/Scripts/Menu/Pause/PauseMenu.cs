f765cb5db0eefc2e93d36df88ec2b4c8c7927e78
Loann <courcoul66@gmail.com>
2022-06-15 11:39:56 +0200

    Add sound on menu

--- SquibblesMania/Assets/Scripts/Menu/Pause/PauseMenu.cs
+++ SquibblesMania/Assets/Scripts/Menu/Pause/PauseMenu.cs
@@ -101,6 +101,7 @@ public class PauseMenu : MonoBehaviour
     public void RotateUi()
     {
 	    AudioManager.Instance.Play("Button");
+	    
         panelPause.transform.rotation *= Quaternion.Euler(0, 0, 180);
     }
 
